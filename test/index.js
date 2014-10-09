a3ad3917bdd373a301fd6b141c2d187f454d4bd7
Justin Gallardo <justin.gallardo@rackspace.com>
2014-10-08 20:33:50 -0700

    Added test for and filter.

--- test/index.js
+++ test/index.js
@@ -40,3 +40,35 @@ test('test simple rule', function(t) {
   r.push({'bar': 'foo'});
   r.push(null);
 });
+
+
+test('test and rule', function(t) {
+  var r = new TestStream,
+      lf = new LogicFilter(),
+      counter = 0;
+
+  lf.add('simpleRule1', {
+    and: {
+      'foo': 'bar',
+      'bar': 'qux'
+    }
+  });
+
+  lf.on('data', function(obj) {
+    counter++;
+  });
+
+  lf.on('end', function() {
+    t.equal(counter, 2, '2 objects passed through the filter');
+    t.end();
+  });
+
+  r.pipe(lf);
+
+  r.push({'foo': 'bar', 'bar': 'qux'});
+  r.push({'foo': 'baz', 'bar': 'foo'});
+  r.push({'foo': 'qux', 'baz': 'qux'});
+  r.push({'foo': 'bar', 'bar': 'qux'});
+  r.push({'bar': 'foo', 'qux': 'bar'});
+  r.push(null);
+});
